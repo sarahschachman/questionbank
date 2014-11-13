f61ed1ba74d40b39c9131579fb7ed449eb6edec4
Sarah Schachman <sarahschachman@college.harvard.edu>
2014-11-13 14:58:15 -0500

    sending json over wire

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,8 +1,12 @@
 class AnswersController < ApplicationController
+  
   def create
     answer = Answer.new(params.require(:answer)
       .permit(:text))
-    add_answer(answer)
+    question = Question.find(params[:id])
+    question.answers << answer
+    question.save
+    render json: question.to_json
   end
 
 end
