7b438c097addd8a2e86da038a6a0f4350e8f2def
Sarah Schachman <sarahschachman@college.harvard.edu>
2014-12-08 11:10:19 -0500

    added user_id to question model

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,7 +3,7 @@ class AnswersController < ApplicationController
   def create
     answer = Answer.new(params.require(:answer)
       .permit(:text))
-    question = Question.find(params[:id])
+    question = Question.find(params[:question_id])
     question.answers << answer
     question.save
     render json: question.to_json
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141101141658) do
+ActiveRecord::Schema.define(version: 20141208140600) do
 
   create_table "answers", force: true do |t|
     t.datetime "created_at"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20141101141658) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "text"
+    t.integer  "user_id"
   end
 
 end
